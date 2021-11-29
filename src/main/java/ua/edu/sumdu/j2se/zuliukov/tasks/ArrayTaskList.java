f08470f9158ad8dfaef743b26ace625902bee900
daredevil2033 <zulyukovivan@gmail.com>
2021-11-29 16:10:54 +0200

    Iterators equals hashCode toString clone style changes

--- src/main/java/ua/edu/sumdu/j2se/zuliukov/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/zuliukov/tasks/ArrayTaskList.java
@@ -1,10 +1,12 @@
 package ua.edu.sumdu.j2se.zuliukov.tasks;
 
-public class ArrayTaskList extends AbstractTaskList{
+import java.util.Iterator;
+
+public class ArrayTaskList extends AbstractTaskList implements Cloneable {
     private Task[] tasks;
 
     public void add(Task task) throws IllegalArgumentException {
-        if(task==null)throw new IllegalArgumentException("Task cannot be null");
+        if (task == null) throw new IllegalArgumentException("Task cannot be null");
         else {
             if (size == 0) {
                 tasks = new Task[++size];
@@ -17,14 +19,13 @@ public class ArrayTaskList extends AbstractTaskList{
         }
     }
 
-    public boolean remove(Task task){
+    public boolean remove(Task task) {
         for (int i = 0; i < size; i++) {
-            if(tasks[i].equals(task)){
-                if(size==1){
-                    tasks=null;
+            if (tasks[i].equals(task)) {
+                if (size == 1) {
+                    tasks = null;
                     size--;
-                }
-                else{
+                } else {
                     Task[] temp = tasks;
                     tasks = new Task[--size];
                     System.arraycopy(temp, 0, tasks, 0, i);
@@ -41,11 +42,99 @@ public class ArrayTaskList extends AbstractTaskList{
     }
 
     public Task getTask(int index) throws IndexOutOfBoundsException {
-        if(index<0||index>size-1)throw new IndexOutOfBoundsException();
+        if (index < 0 || index > size - 1) throw new IndexOutOfBoundsException();
         else return tasks[index];
     }
 
     public ArrayTaskList() {
-        super(0,ListTypes.types.ARRAY);
+        super(0, ListTypes.types.ARRAY);
+    }
+
+    class ArrayTaskListIterator implements Iterator<Task> {
+        int current;
+
+        public ArrayTaskListIterator() {
+            current = -1;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != size - 1;
+        }
+
+        @Override
+        public Task next() {
+            return tasks[++current];
+        }
+
+        @Override
+        public void remove() {
+            if (current == -1) throw new IllegalStateException();
+            else {
+                if (size == 1) {
+                    tasks = null;
+                    size--;
+                } else {
+                    Task[] temp = tasks;
+                    tasks = new Task[--size];
+                    System.arraycopy(temp, 0, tasks, 0, current);
+                    System.arraycopy(temp, current + 1, tasks, current, size - current);
+                }
+                current--;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        return new ArrayTaskListIterator();
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) return true;
+        if (otherObject == null || getClass() != otherObject.getClass()) return false;
+        ArrayTaskList other = (ArrayTaskList) otherObject;
+        if (other.size() != size) return false;
+        boolean eq = true;
+        for (int i = 0; i < size; i++) {
+            if (!other.getTask(i).equals(tasks[i])) {
+                eq = false;
+                break;
+            }
+        }
+        return eq;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            result += tasks[i].hashCode();
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ArrayTaskList{");
+        sb.append("size=").append(size);
+        sb.append(", type=").append(type);
+        sb.append(", tasks=");
+        for (int i = 0; i < size; i++) {
+            sb.append(tasks[i]);
+            if (i != size - 1) sb.append(", ");
+        }
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public ArrayTaskList clone() {
+        ArrayTaskList clone = new ArrayTaskList();
+        for (int i = 0; i < size; i++) {
+            clone.add(this.tasks[i].clone());
+        }
+        return clone;
     }
 }
--- src/main/java/ua/edu/sumdu/j2se/zuliukov/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/zuliukov/tasks/Task.java
@@ -1,6 +1,6 @@
 package ua.edu.sumdu.j2se.zuliukov.tasks;
 
-public class Task {
+public class Task implements Cloneable {
     private String title;
     private int time;
     private int start;
@@ -11,7 +11,7 @@ public class Task {
 
     public Task(String title, int time) throws IllegalArgumentException {
         this.title = title;
-        if(time<0)throw new IllegalArgumentException("Time elapsed cannot be less than 0");
+        if (time < 0) throw new IllegalArgumentException("Time elapsed cannot be less than 0");
         else this.time = time;
         this.active = false;
         this.repeated = false;
@@ -19,14 +19,14 @@ public class Task {
 
     public Task(String title, int start, int end, int interval) throws IllegalArgumentException {
         this.title = title;
-        if(start<0)throw new IllegalArgumentException("Start cannot be less than 0");
-        else if(end<=0)throw new IllegalArgumentException("End must be greater than 0");
-        else if(start>end)throw new IllegalArgumentException("Start cannot be after End");
-        else{
+        if (start < 0) throw new IllegalArgumentException("Start cannot be less than 0");
+        else if (end <= 0) throw new IllegalArgumentException("End must be greater than 0");
+        else if (start > end) throw new IllegalArgumentException("Start cannot be after End");
+        else {
             this.start = start;
             this.end = end;
         }
-        if(interval<=0)throw new IllegalArgumentException("Interval must be greater than 0");
+        if (interval <= 0) throw new IllegalArgumentException("Interval must be greater than 0");
         else this.interval = interval;
         this.active = false;
         this.repeated = true;
@@ -49,40 +49,40 @@ public class Task {
     }
 
     public int getTime() {
-        if(repeated) return start;
+        if (repeated) return start;
         else return time;
     }
 
     public void setTime(int time) throws IllegalArgumentException {
-        if(time<0)throw new IllegalArgumentException("Time elapsed cannot be less than 0");
+        if (time < 0) throw new IllegalArgumentException("Time elapsed cannot be less than 0");
         else this.time = time;
         this.repeated = false;
     }
 
     public int getStartTime() {
-        if(repeated) return start;
+        if (repeated) return start;
         else return time;
     }
 
     public int getEndTime() {
-        if(repeated) return end;
+        if (repeated) return end;
         else return time;
     }
 
     public int getRepeatInterval() {
-        if(repeated) return interval;
+        if (repeated) return interval;
         else return 0;
     }
 
     public void setTime(int start, int end, int interval) throws IllegalArgumentException {
-        if(start<0)throw new IllegalArgumentException("Start cannot be less than 0");
-        else if(end<=0)throw new IllegalArgumentException("End must be greater than 0");
-        else if(start>end)throw new IllegalArgumentException("Start cannot be after End");
-        else{
+        if (start < 0) throw new IllegalArgumentException("Start cannot be less than 0");
+        else if (end <= 0) throw new IllegalArgumentException("End must be greater than 0");
+        else if (start > end) throw new IllegalArgumentException("Start cannot be after End");
+        else {
             this.start = start;
             this.end = end;
         }
-        if(interval<=0)throw new IllegalArgumentException("Interval must be greater than 0");
+        if (interval <= 0) throw new IllegalArgumentException("Interval must be greater than 0");
         else this.interval = interval;
         this.repeated = true;
     }
@@ -91,24 +91,64 @@ public class Task {
         return repeated;
     }
 
-    public int nextTimeAfter(int current){
-        if(active){
+    public int nextTimeAfter(int current) {
+        if (active) {
             if (repeated) {
-                if(current < start)return start;
+                if (current < start) return start;
                 else {
                     int nextTime = start + interval;
-                    while(nextTime < end){
-                        if(current < nextTime){
+                    while (nextTime < end) {
+                        if (current < nextTime) {
                             return nextTime;
                         }
-                        nextTime+=interval;
+                        nextTime += interval;
                     }
                     return -1;
                 }
-            }
-            else if (current < time) return time;
+            } else if (current < time) return time;
             else return -1;
+        } else return -1;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) return true;
+        if (otherObject == null || getClass() != otherObject.getClass()) return false;
+        Task other = (Task) otherObject;
+        if (time != other.time) return false;
+        if (start != other.start) return false;
+        if (end != other.end) return false;
+        if (interval != other.interval) return false;
+        if (active != other.active) return false;
+        if (repeated != other.repeated) return false;
+        return title.equals(other.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode() + time + start + end + interval + (active ? 1 : 0) + (repeated ? 1 : 0);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Task{");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", time=").append(time);
+        sb.append(", start=").append(start);
+        sb.append(", end=").append(end);
+        sb.append(", interval=").append(interval);
+        sb.append(", active=").append(active);
+        sb.append(", repeated=").append(repeated);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public Task clone() {
+        try {
+            return (Task) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
         }
-        else return -1;
     }
 }
